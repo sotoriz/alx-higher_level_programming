eb5015f17ea7b76c893854179a31f4544f088ef4
sotoriz <sotorisfx@gmail.com>
2023-04-25 15:02:35 -0700

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-
 const request = require('request');
-const URL = 'https://swapi-api.alx-tools.com/api/films/:id';
-const episode = process.argv[2];
-
-request(URL + episode, function (error, response, body) {
-  error && console.log(error);
-  console.log(JSON.parse(body).title);
+let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
